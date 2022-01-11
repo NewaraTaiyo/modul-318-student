e314c77a16cf6fab797323364e8114081d5f2e26
NewaraTaiyo <julia@stadler.li>
2022-01-11 11:03:43 +0100

    add getConnectionsBetweenStations

--- MyTransportApp/JourneyConnections.cs
+++ MyTransportApp/JourneyConnections.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SwissTransport;
+using System.string;
+using System.Object;
+using System.List;
 
 namespace MyTransportApp {
     public class JourneyConnections {
-        ITransport swissTransport = new Transport();
+        private ITransport transport = new Transport();
+
+        public List<Connections> getConnectionsBetweenStations(string startStation, string endStation) {
+            return this.transport.getConnections(startStation, endStation); ;
+        }
     }
 }
